21cea314a2c59ada1baeacf469e2663661951992
AnuragRawat92 <148265902+AnuragRawat92@users.noreply.github.com>
2024-06-18 07:46:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,40 +1,37 @@
-// Importing required modules
-import swaggerui from "swagger-ui-express";
-import swaggerdoc from "swagger-jsdoc";
-import express from "express";
-import 'express-async-errors';
-import dotenv from "dotenv";
-import colors from "colors";
-import cors from "cors";
-import morgan from "morgan";
-import helmet from "helmet";
-import xss from "xss-clean";
-import mongosanitize from "express-mongo-sanitize";
+import express from 'express';
+import cors from 'cors';
+import swaggerui from 'swagger-ui-express';
+import swaggerdoc from 'swagger-jsdoc';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import xss from 'xss-clean';
+import mongosanitize from 'express-mongo-sanitize';
+import colors from 'colors';
+import morgan from 'morgan';
 
-// Importing local files
-import connectdb from "./config/db.js";
-import testroutes from "./routes/testroute.js";
-import authRoutes from "./routes/authRoutes.js";
-import errorMiddleware from "./middlewares/errorMiddlewares.js";
-import userRoutes from "./routes/userRoutes.js";
-import jobRoutes from "./routes/jobsRoutes.js";
+import connectdb from './config/db.js';
+import testroutes from './routes/testroute.js';
+import authRoutes from './routes/authRoutes.js';
+import errorMiddleware from './middlewares/errorMiddlewares.js';
+import userRoutes from './routes/userRoutes.js';
+import jobRoutes from './routes/jobsRoutes.js';
 
-// dotenv configuration
+// dotenv config
 dotenv.config();
 
-// Swagger configuration options
+// config swagger api
 const options = {
   definition: {
-    openapi: "3.0.0",
+    openapi: '3.0.0',
     info: {
-      title: "Job Portal Application",
-      description: "NodeJs ExpressJs Job Portal Application"
+      title: 'Job Portal Application',
+      description: 'NodeJs ExpressJs Job Portal Application',
     },
     servers: [
       {
-        url: "http://localhost:8080",
+        url: 'http://localhost:8080',
         description: 'Local server',
-      }
+      },
     ],
     components: {
       securitySchemes: {
@@ -46,42 +43,41 @@ const options = {
       },
     },
   },
-  apis: ['./routes/*.js']
+  apis: ['./routes/*.js'],
 };
 
 const spec = swaggerdoc(options);
 
-// Connect to MongoDB
+// mongodb connection
 connectdb();
 
-// Initialize express application
+// rest objects
 const app = express();
 
-// Middlewares
+// middlewares
 app.use(helmet());
 app.use(xss());
 app.use(mongosanitize());
 app.use(express.json());
-app.use(cors());
+app.use(cors()); // Enable CORS
 app.use(morgan('dev'));
 
-// Routes
+// routes
 app.use('/api/v1/test', testroutes);
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/user', userRoutes);
 app.use('/api/v1/job', jobRoutes);
 app.use('/api-doc', swaggerui.serve, swaggerui.setup(spec));
 
-// Error handling middleware
+// error handling middleware
 app.use(errorMiddleware);
 
-// Redirect root to Swagger UI
-// app.get('/', (req, res) => {
-//   res.redirect('/api-doc');
-// });
-app.options('*', cors());
+// redirect root to api-doc
+app.get('/', (req, res) => {
+  res.redirect('/api-doc');
+});
 
-// Start the server
+// listen
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.Dev_Mode} mode at ${PORT}`.bgCyan.white);
